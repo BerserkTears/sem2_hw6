8554f91957e695f3614c88c244be03f04040d56a
BerserkTears <kirill.kunitsyn@gmail.com>
2022-05-06 00:51:24 +0300

    did?(2)

--- main.cpp
+++ main.cpp
@@ -1,38 +1,32 @@
 #include <iostream>
-#include "istream"
 
 class polynomial{
 private:
     const int m_amount;
     const int * m_coefficients;
-    static constexpr int pow(const int number, const int degree) {
-        int result = 1;
-        for (int i = 0; i <= degree; ++i) {
-            result *= number;
-        }
-        return result;
+    constexpr int pow(const int number, const int degree) const{
+        return degree == 0 ? 1 : number * pow(number, degree - 1);
+    }
+    constexpr int calc (const int point, const int degree) const {
+        return degree == 0 ? 0 : m_coefficients[m_amount - degree] * pow(point, m_amount - degree) + calc(point, degree - 1);
     }
 public:
       constexpr polynomial(const int amount, const int coefficients[]):m_amount(amount),m_coefficients(coefficients){
-//        m_coefficients = new int[amount];
-//        for (int i = 0; i < amount; ++i) {
-//            m_coefficients[i] = coefficients[i];
-//        }
-//    m_coefficients = coefficients;
     }
+
+    ~polynomial(){
+        delete [] m_coefficients;
+    }
+
     constexpr int solve(const int point) const{
-        int sum = 0;
-        for (int i = 0; i < m_amount; ++i) {
-            sum+= m_coefficients[i] * pow(point, i);
-        }
-        return sum / point;
+        return calc(point, m_amount);
       }
 };
 
 int main() {
     constexpr int amount = 3;
-    constexpr int vec[] = {3,2,3};
+    constexpr int vec[] = {3,3, 3};
     const polynomial poly(amount, vec);
-    std::cout << poly.solve(50);
+    std::cout << poly.solve(5);
     return 0;
 }
